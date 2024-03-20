c603aed8c08e84a862537c188dfa9144c0667fb6
Abdelrahman125 <abdelrahman24689@gmail.com>
2024-03-20 23:37:15 +0200

    Modifying AppUI

--- src/main/java/productivitychecckerapp/AppUI.java
+++ src/main/java/productivitychecckerapp/AppUI.java
@@ -1,16 +1,6 @@
 package productivitychecckerapp;
 
 public class AppUI {
-    public static void main(String[]args){
-       Todo todo1 = new Todo("First Todo");
-       todo1.setDescription("This good description");
-       Task task1 = new Task("Study for Quiz","15 March");
-       Task task2 = new Task("Study for Midterm", "1 April");
-       todo1.addTask(task1);
-       todo1.addTask(task2);
 
-       System.out.println(todo1.getTask(0).getTaskText());
-       // new comment
 
-    }
 }
