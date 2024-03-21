0cd4ed51fa4f50d7af0333f0862bd5e5e343ee05
Abdelrahman125 <abdelrahman24689@gmail.com>
2024-03-21 21:54:53 +0200

    Created readDb method

--- src/main/java/productivitychecckerapp/Main.java
+++ src/main/java/productivitychecckerapp/Main.java
@@ -5,10 +5,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.util.Objects;
 
 public class Main extends Application {
+    static String UserName = "Abdelrahman125"; // will be changed later
+    public static void readDb(){
+        ArrayList<Todo> todos = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(Main.class.getResource("Todos.csv").getPath()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // assign data to objects
+                String[] dataFields = line.split(",");  // Split by comma
+                if (Objects.equals(dataFields[0], UserName) || Objects.equals(dataFields[0], "Fares")){
+                    Todo todo = new Todo(dataFields[2]);
+                    todos.add(todo);
+                }
+            }
+        } catch (IOException e) {
+            // Handle exception
+            e.printStackTrace();
+        }
+        System.out.println(todos.get(0).getTitle());
+        System.out.println(todos.get(1).getTitle());
 
+
+    }
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
@@ -19,6 +45,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        readDb();
         launch();
     }
 }
