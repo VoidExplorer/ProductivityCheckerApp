a96f2380733b675771e4e35a4333b4051a838961
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-04-08 23:32:09 +0200

    feat: add ability to read signup fields details

--- src/main/java/productivitychecckerapp/SceneController.java
+++ src/main/java/productivitychecckerapp/SceneController.java
@@ -1,10 +1,12 @@
 package productivitychecckerapp;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
@@ -20,5 +22,18 @@ public class SceneController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private void signup(ActionEvent event) {
+
+        System.out.println(usernameField.getText());
+        System.out.println(passwordField.getText());
+
     }
 }
