c73e67f02256e2ecc681bd7458c19f03930206d6
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-04-09 01:01:53 +0200

    chore(deps): add javafx.media

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module productivitychecckerapp.demo {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.media;
 
     opens productivitychecckerapp to javafx.fxml;
     exports productivitychecckerapp;
