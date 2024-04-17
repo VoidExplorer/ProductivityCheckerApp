e7d0c5baea858bd09db87ec5da4323c2bc7474e4
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-04-17 07:08:07 +0200

    refactor: move INSERT_USER inside addUser method

--- src/main/java/productivitycheckerapp/database.java
+++ src/main/java/productivitycheckerapp/database.java
@@ -5,7 +5,6 @@ import java.sql.*;
 public class database {
 
     static Connection connection;
-    private static final String INSERT_SQL = "INSERT INTO USERS(UserName, Password) VALUES(?, ?)";
 
     public static void connect() throws SQLException {
         try {
@@ -17,8 +16,9 @@ public class database {
     }
 
     public static void addUser(String userName, String password) throws SQLException {
+        String INSERT_USER = "INSERT INTO users(UserName, Password) VALUES(?, ?)";
         int nRows = 0;
-        PreparedStatement ps = connection.prepareStatement(INSERT_SQL);
+        PreparedStatement ps = connection.prepareStatement(INSERT_USER);
         Statement statement = connection.createStatement();
         ps.setString(1, userName);
         ps.setString(2, password);
