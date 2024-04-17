467e4eac4bd8e1d3569aab8159f8212aa38b1348
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-04-17 05:51:06 +0200

    chore(deps): add sqlite-jdbc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module productivitychecckerapp.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires java.sql;
 
     opens productivitycheckerapp to javafx.fxml;
     exports productivitycheckerapp;
