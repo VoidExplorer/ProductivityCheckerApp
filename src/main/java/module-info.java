bb9f25739a473c4bf1958562ada5322ee9837847
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-04-18 22:20:56 +0200

    chore(deps): add fontawesomefx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module productivitychecckerapp.demo {
     requires javafx.fxml;
     requires javafx.media;
     requires java.sql;
+    requires de.jensd.fx.glyphs.commons;
+    requires de.jensd.fx.glyphs.materialdesignicons;
+    requires de.jensd.fx.glyphs.fontawesome;
 
     opens productivitycheckerapp to javafx.fxml;
     exports productivitycheckerapp;
