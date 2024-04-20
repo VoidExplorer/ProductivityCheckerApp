4b23bfb287f33c894653359cb215b4d38f77d9f6
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-04-20 17:40:52 +0200

    feat: add hamburger menu to signup.fxml

--- src/main/java/controllers/SceneController.java
+++ src/main/java/controllers/SceneController.java
@@ -1,22 +1,30 @@
 package controllers;
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import productivitycheckerapp.database;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class SceneController{
+public class SceneController implements Initializable {
     private Stage stage;
     private Scene scene;
 
@@ -40,7 +48,7 @@ public class SceneController{
         database.addUser(username, password);
     }
 
-    public void switchToSettings(MouseEvent event) throws IOException {
+    public void switchToSettings(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("settings.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -65,6 +73,37 @@ public class SceneController{
         popup.show();
     }
 
+    @FXML
+    private JFXHamburger menu;
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private JFXDrawer drawer;
+
+    @FXML
+    private VBox drawerPane;
+
+    @FXML
+    private AnchorPane drawerBack;
+    @FXML
+    private VBox drawerPaneTest;
+    private static boolean visible;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        drawerBack.setVisible(visible);
+        HamburgerBasicCloseTransition hbct = new HamburgerBasicCloseTransition(menu);
+        hbct.setRate(-1);
+        menu.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+            visible = !visible;
+            boolean vi = true;
+            hbct.setRate(hbct.getRate()*-1);
+            hbct.play();
+            drawerBack.setVisible(visible);
+        });
+
+    }
 
 
 }
