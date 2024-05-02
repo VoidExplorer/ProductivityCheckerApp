3a367d57e9f2d451ac922a422c5e70f54da8731d
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-05-02 20:36:50 +0300

    feat: add methods to change color of fields based on a limit

--- src/main/java/controllers/SceneController.java
+++ src/main/java/controllers/SceneController.java
@@ -3,6 +3,7 @@ package controllers;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -25,8 +27,33 @@ import java.sql.SQLException;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
+/* adds a listener that checks if the text in "field" exceeds the "limit"
+   and uses "label" to change the color of the label to red if that's true, reverts to purple otherwise
+ */
 public class SceneController {
+    public static void setTextFieldLimit(MFXTextField field, Label label, int limit) {
+        field.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (observable.getValue().length() > limit) {
+                label.setVisible(true);
+                field.setStyle("-fx-border-color: red");
+            } else {
+                label.setVisible(false);
+                field.setStyle("-fx-border-color: #594BE8");
+            }
+        });
+    }
 
+    public static void setPasswordFieldLimit(MFXTextField field, Label label, int limit) {
+        field.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (observable.getValue().length() > limit) {
+                label.setVisible(true);
+                field.setStyle("-fx-border-color: red");
+            } else {
+                label.setVisible(false);
+                field.setStyle("-fx-border-color: #594BE8");
+            }
+        });
+    }
 
 }
 
