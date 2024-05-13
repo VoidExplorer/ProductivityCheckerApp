896de493f36a1d77ce7a46f7367de16d805aea4b
Abdelrahman125 <abdelrahman24689@gmail.com>
2024-05-14 02:50:28 +0300

    Now you can see your todo

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,4 +1,5 @@
 package controllers;
+
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,11 +9,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import productivitycheckerapp.MFXResourcesLoader;
 import productivitycheckerapp.Todo;
+
 import java.io.IOException;
+import javafx.scene.control.Label;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -21,10 +23,18 @@ import static controllers.SceneController.scene;
 
 public class HomeController implements Initializable {
 
-    static VBox todosbox_;
+    static VBox todosbox_; // idk what is that blame fares (faris)
     @FXML
     private VBox todosbox;
     private Stage stage;
+
+    @FXML
+    private Label TODOTITLE;
+    @FXML
+    private Label TODODESC;
+    @FXML
+    private static HBox hbox1;
+
 
     public void refreshTodos(ActionEvent e){
        todosbox.getChildren().clear();
@@ -48,30 +58,33 @@ public class HomeController implements Initializable {
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+
     }
 
-    private static MFXButton getButton(String todoTitle) {
+    private MFXButton getButton(String todoTitle) {
         MFXButton button = new MFXButton(todoTitle);
         button.setOnMouseClicked(mouseEvent -> {
-            try {
-                FXMLLoader loader = new FXMLLoader(MFXResourcesLoader.loadURL("TodoPageScreen.fxml"));
-                Parent root = loader.load();
-
-                Stage stage = (Stage) button.getScene().getWindow();
-                scene = new Scene(root, scene.getWidth(), scene.getHeight());
-                stage.setScene(scene);
-                stage.show();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
+            TODOTITLE.setText("Hello tttttest");
+            TODODESC.setText("fucking good description nothing better than this description");
         });
-        return button;
-    }
 
+        return button;
+
+    }
+    public void EditTodo(ActionEvent e){
+        // do nothing
+    }
+    public void deleteTodo(ActionEvent e){
+        //good
+    }
+    public void switchToAddTask(ActionEvent e){
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ActionEvent onload = new ActionEvent();
         refreshTodos(onload);
         todosbox_ = todosbox;
+
     }
 }
