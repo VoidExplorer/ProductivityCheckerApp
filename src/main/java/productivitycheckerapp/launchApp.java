4bb65f51a10faed025a3160832bc897aed645c46
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-05-14 22:13:49 +0300

    feat: add jar build configuration

---
+++ src/main/java/productivitycheckerapp/launchApp.java
@@ -0,0 +1,9 @@
+package productivitycheckerapp;
+
+import java.sql.SQLException;
+
+public class launchApp {
+    public static void main(String[] args) throws SQLException {
+        Main.main(args);
+    }
+}
