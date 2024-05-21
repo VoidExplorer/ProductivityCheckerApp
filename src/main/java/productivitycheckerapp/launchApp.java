3b10ce0edd6b21c9b56786121ab6db25e61dea13
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-05-21 09:39:53 +0300

    refactor: remove redundant code in launchApp.java

--- src/main/java/productivitycheckerapp/launchApp.java
+++ src/main/java/productivitycheckerapp/launchApp.java
@@ -2,16 +2,9 @@ package productivitycheckerapp;
 
 import java.awt.*;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
+import java.text.ParseException;
 public class launchApp {
-    public static void main(String[] args) throws SQLException, AWTException {
-        NotificationManager notificationManager = new NotificationManager("tip");
-        notificationManager.sendNotification("في حالة العثور على اي مشكلة", "رستر الراوتر");
-        LocalTime d = LocalTime.now();
-        System.out.println(d);
+    public static void main(String[] args) throws SQLException, AWTException, ParseException {
         Main.main(args);
     }
 }
